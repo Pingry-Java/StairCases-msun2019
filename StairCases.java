af05e1518fe667ac9c45933fc2e760bc04e913e5
Michael Sun <msun2019@pingry.org>
2016-09-25 10:20:52 -0400

    Finished program

--- StairCases.java
+++ StairCases.java
@@ -1,7 +1,11 @@
+
 /**
- * Write javadoc for your class here (and remove this comment, or course)
+ * This class contains 2 methods that print ascending and descending staircases 
  *
+ * @author Michael Sun
+ * @version 1.0 
  */
+import java.util.Scanner;
 
 public class StairCases
 {
@@ -13,21 +17,48 @@ public class StairCases
   public static void main(String[] args)
   {
     //TODO change the next line so it actually gets input from the user
-    int size = 4;
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("How many stairs would you like to have? ");
+    int size = keyboard.nextInt();
 
     printAscending(size);
     printDescending(size);
   }
 
   /**
-   * Write your javadoc here
+   * This method prints an ascending staircase 
+   * @param size The amount of stairs there will be, this variable is inputed by the user 
    */
-  public static void printAscending
+  public static void printAscending(int size)
   //TODO Finish this method
+  {
+   for (int counter = 0; counter < size; counter++)
+   {
+    for (int stars = 0; stars < counter + 1; stars++)
+    {
+     System.out.print("*");
+    }
+   
+    System.out.print("\n");
+   }
+  }
 
   /**
-   * Write your javadoc here
+   * This method prints a descending staircase
+   * @param size The amount of stairs there will be, this variable is inputed by the user 
    */
-  public static void printDescending
+  public static void printDescending(int size)
+  {
   //TODO Finish this method
+   System.out.println("\n");
+   for (int counter = size; counter > 0; counter--)
+   {
+    for (int stars = 0; stars < counter; stars ++)
+    {
+     System.out.print("*");
+    }
+    System.out.print("\n"); 
+   }
+  }
+
 }
